bb5d99511284c44c9ff46dc88d9799651d388525
AdyPon <32988161+AdyPon@users.noreply.github.com>
2022-11-26 04:59:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm.asyncio import tqdm
-from fake_useragent import UserAgent
 import csv
 from pathlib import Path
 from itertools import cycle
@@ -9,65 +8,81 @@ from proxy_parser import proxy_parser
 import asyncio
 import aiohttp
 from aiohttp_socks import ProxyConnector
+from aiohttp_retry import RetryClient, ExponentialRetry
 
 
 class HabrParser:
-    def __init__(self, urls_file, proxies, limit=None):
+    def __init__(self, urls_file: str, proxies: list, limit=None):
+        """
+        urls_file (str): файл с ссылками на профили
+        proxies (list): список прокси
+        limit (int): ограничение количества ссылок
+        """
         self.links = pd.read_csv(urls_file)
         if limit:
             self.links = self.links.iloc[:limit]
         self.proxies = cycle(proxies)
 
-    def start(self, continue_parsing=False, successful_links='successful_links.csv'):
+    def start(self, continue_parsing=False, successful_links='successful_links.csv') -> None:
+        """
+        Запускает парсинг.
+        continue_parsing (bool): True - если парсинг был прерван и его нужно продолжить,
+                                 False - если парсинг нужно начать заново.
+        successful_links (str): Файл с ссылками, которые не нужно использовать.
+        """
         print('Parsing has started!')
         asyncio.get_event_loop().run_until_complete(self.get_all_pages(continue_parsing, successful_links))
         print('Parsing is finished!')
 
     async def get_page_handler(self, link, session, pbar):
-        headers = {
-            'accept': '*/*',
-            'user-agent': UserAgent().chrome
-        }
-
         try:
             await asyncio.sleep(0.1)
-            req = await session.get(link, headers=headers, timeout=2)
-            req_status = req.status
+            retry_options = ExponentialRetry(attempts=5, max_timeout=2)
+            retry_client = RetryClient(session)
+            async with retry_client.get(link, retry_options=retry_options) as req:
+                req_status = req.status
+                if req_status == 200:
+                    src = await req.text()
+                    soup = BeautifulSoup(src, 'lxml')
 
-            if req_status == 200:
-                src = await req.text()
-                soup = BeautifulSoup(src, 'lxml')
+                    user_name = soup.find('h1', class_='page-title__title').text.strip()
 
-                user_name = soup.find('h1', class_='page-title__title').text.strip()
+                    new_user_name = user_name
+                    for i in [' ', '*']:
+                        new_user_name = new_user_name.replace(i, '_')
 
-                new_user_name = user_name.replace(' ', '_')
+                    path = Path('pages', new_user_name + '.html')
 
-                path = Path('pages', new_user_name + '.html')
+                    with open(path, 'w', encoding="utf-8") as file:
+                        file.write(link + '\n')
+                        file.write(user_name + '\n')
+                        file.write(src)
 
-                with open(path, 'w', encoding="utf-8") as file:
-                    file.write(link + '\n')
-                    file.write(user_name + '\n')
-                    file.write(src)
+                    with open('successful_links.csv', 'a', newline='') as csvfile:
+                        successful_links_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                        successful_links_writer.writerow([link])
 
-                with open('successful_links.csv', 'a', newline='') as csvfile:
-                    successful_links_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-                    successful_links_writer.writerow([link])
+                    pbar.update(1)
+                    return {'status_code': True, 'link': link}
+                elif req_status == 404:
+                    pbar.update(1)
 
-                pbar.update(1)
-                return {'status_code': True, 'link': link}
-            elif req_status == 404:
-                pbar.update(1)
-
-                with open('successful_links.csv', 'a', newline='') as csvfile:
-                    successful_links_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-                    successful_links_writer.writerow([link])
-                return {'status_code': True, 'link': link}
-            elif req_status == 429:
-                return {'status_code': False, 'link': link}
+                    with open('successful_links.csv', 'a', newline='') as csvfile:
+                        successful_links_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                        successful_links_writer.writerow([link])
+                    return {'status_code': True, 'link': link}
+                elif req_status == 429:
+                    return {'status_code': False, 'link': link}
         except:
             return {'status_code': False, 'link': link}
 
-    async def get_all_pages(self, continue_parsing, successful_links):
+    async def get_all_pages(self, continue_parsing: bool, successful_links: str) -> None:
+        """
+        Метод получения всех страниц.
+        continue_parsing (bool): True - если парсинг был прерван и его нужно продолжить,
+                                 False - если парсинг нужно начать заново.
+        successful_links (str): Файл с ссылками, которые не нужно использовать.
+        """
         try:
             Path('pages').mkdir()
         except FileExistsError:
@@ -115,12 +130,13 @@ class HabrParser:
                     break
 
 
-my_proxies = ['socks5://w45L39:MV7Ue8@194.67.219.107:9750',
-              'socks5://testvicky2:2a31eb@193.23.50.202:10486',
-              'socks5://VSwLsQ:sEEUU3@194.28.208.64:9824']
+if __name__ == "__main__":
+    my_proxies = ['socks5://w45L39:MV7Ue8@194.67.219.107:9750',
+                  'socks5://testvicky2:2a31eb@193.23.50.202:10486',
+                  'socks5://VSwLsQ:sEEUU3@194.28.208.64:9824']
 
-proxies = proxy_parser()
+    proxies = proxy_parser()
 
-parser = HabrParser('habr.csv', proxies, limit=100)
+    parser = HabrParser('habr.csv', my_proxies + proxies)
 
-parser.start()
+    parser.start(continue_parsing=True)
