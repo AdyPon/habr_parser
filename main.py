c652d2563dfe628bd6f264fd22f351f6cae3cb3c
AdyPon <adypon@mail.ru>
2022-11-28 03:43:33 +0300

    some change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fake_useragent import UserAgent
 import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm.asyncio import tqdm
@@ -12,7 +13,7 @@ from aiohttp_retry import RetryClient, ExponentialRetry
 
 
 class HabrParser:
-    def __init__(self, urls_file: str, proxies: list, limit=None):
+    def __init__(self, urls_file: str, proxies: list, limit=None, n_coroutines=50):
         """
         urls_file (str): файл с ссылками на профили
         proxies (list): список прокси
@@ -22,6 +23,7 @@ class HabrParser:
         if limit:
             self.links = self.links.iloc[:limit]
         self.proxies = cycle(proxies)
+        self.n_coroutines = n_coroutines
 
     def start(self, continue_parsing=False, successful_links='successful_links.csv') -> None:
         """
@@ -31,13 +33,12 @@ class HabrParser:
         successful_links (str): Файл с ссылками, которые не нужно использовать.
         """
         print('Parsing has started!')
-        asyncio.get_event_loop().run_until_complete(self.get_all_pages(continue_parsing, successful_links))
+        asyncio.run(self.get_all_pages(continue_parsing, successful_links))
         print('Parsing is finished!')
 
     async def get_page_handler(self, link, session, pbar):
         try:
-            await asyncio.sleep(0.1)
-            retry_options = ExponentialRetry(attempts=5, max_timeout=2)
+            retry_options = ExponentialRetry(attempts=10, max_timeout=2)
             retry_client = RetryClient(session)
             async with retry_client.get(link, retry_options=retry_options) as req:
                 req_status = req.status
@@ -63,6 +64,8 @@ class HabrParser:
                         successful_links_writer.writerow([link])
 
                     pbar.update(1)
+                    await session.close()
+
                     return {'status_code': True, 'link': link}
                 elif req_status == 404:
                     pbar.update(1)
@@ -70,10 +73,13 @@ class HabrParser:
                     with open('successful_links.csv', 'a', newline='') as csvfile:
                         successful_links_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
                         successful_links_writer.writerow([link])
+                    await session.close()
                     return {'status_code': True, 'link': link}
                 elif req_status == 429:
+                    await session.close()
                     return {'status_code': False, 'link': link}
         except:
+            await session.close()
             return {'status_code': False, 'link': link}
 
     async def get_all_pages(self, continue_parsing: bool, successful_links: str) -> None:
@@ -102,29 +108,48 @@ class HabrParser:
                 url_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
                 url_writer.writerow(['url'])
 
-        links = set(self.links)
+        links = self.links.copy()
         with tqdm(total=len(links), desc='Parsing progress:') as pbar:
             count = 0
-
             while len(links) != 0:
                 if count <= 50:
-                    count += 1
-                    proxy = next(self.proxies)
-                    connector = ProxyConnector.from_url(proxy)
-                    async with aiohttp.ClientSession(connector=connector) as session:
-                        tasks = []
+                    tasks = []
+                    if len(links) >= self.n_coroutines:
+                        for link in links[:self.n_coroutines]:
+                            proxy = next(self.proxies)
+                            connector = ProxyConnector.from_url(proxy)
+                            headers = {
+                                'accept': '*/*',
+                                'user-agent': UserAgent().chrome
+                            }
+                            timeout = aiohttp.ClientTimeout(connect=5)
+                            session = aiohttp.ClientSession(connector=connector, headers=headers, timeout=timeout)
+                            task = asyncio.create_task(self.get_page_handler(link, session, pbar))
+                            tasks.append(task)
+                    else:
                         for link in links:
+                            proxy = next(self.proxies)
+                            connector = ProxyConnector.from_url(proxy)
+                            headers = {
+                                'accept': '*/*',
+                                'user-agent': UserAgent().chrome
+                            }
+                            timeout = aiohttp.ClientTimeout(connect=5)
+                            session = aiohttp.ClientSession(connector=connector, headers=headers, timeout=timeout)
                             task = asyncio.create_task(self.get_page_handler(link, session, pbar))
                             tasks.append(task)
 
-                        coroutine = await asyncio.gather(*tasks)
-
-                    if len(coroutine) != len(links):
-                        count = 0
-
+                    coroutine = await asyncio.gather(*tasks)
+                    count_removed_links = 0
                     for cor in coroutine:
                         if isinstance(cor, dict) and cor.get('status_code'):
                             links.remove(cor['link'])
+                            count_removed_links += 1
+
+                    if count_removed_links != 0:
+                        count = 0
+                    else:
+                        count += 1
                 else:
                     print('The proxy has been changing for too long')
                     break
@@ -135,8 +160,8 @@ if __name__ == "__main__":
                   'socks5://testvicky2:2a31eb@193.23.50.202:10486',
                   'socks5://VSwLsQ:sEEUU3@194.28.208.64:9824']
 
-    proxies = proxy_parser()
+    # proxies = proxy_parser()
 
-    parser = HabrParser('habr.csv', my_proxies + proxies)
+    parser = HabrParser('habr.csv', my_proxies, n_coroutines=100)
 
     parser.start()
